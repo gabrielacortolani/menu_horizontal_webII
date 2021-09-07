a795d32a46c43715d34c0907b3d418f693fc9d2b
gabrielacortolani <cardoso.gabicardoso@gmail.com>
2021-09-07 20:02:14 -0300

    menu_horizontal

---
+++ menu/src/componentes/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Menu from './Menu_horizontal.js';
+import menu from "./Menu_horizontal.js";
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      
+    </div>
+
+  );
+}
+
+export default App;
